732c0e761391d86638701133500085a47cfe1871
pritam-code-n-7 <pritamimca1003@gmail.com>
2024-04-05 09:38:20 +0530

    add lazy , Suspense for lazy loading images

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,46 @@
+import { lazy, Suspense } from "react";
+
 function Header() {
+  const Image = lazy(() => import("./Image"));
   return (
     <div className="flex flex-col sm:flex-row p-4 bg-white justify-between items-center rounded-md">
       <div className="flex items-center mb-4 sm:mb-0">
-        <img src="/assets/DP.jpg" className="me-3 h-7" alt="Profile" />
+        <div>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Image />
+          </Suspense>
+        </div>
         <p className="font-semibold text-sm sm:text-base">Frontend Hire</p>
       </div>
 
       <div className="flex flex-col sm:flex-row sm:gap-6">
         <div className="flex flex-col sm:flex-row gap-4 items-center mb-4 sm:mb-0">
-          <a href="#a" className="text-black hover:text-gray-500 text-xs sm:text-sm">HOW IT WORKS</a>
-          <a href="#b" className="text-black hover:text-gray-500 text-xs sm:text-sm">TESTIMONIALS</a>
-          <a href="#c" className="text-black hover:text-gray-500 text-xs sm:text-sm">PRICING</a>
+          <a
+            href="#a"
+            className="text-black hover:text-gray-500 text-xs sm:text-sm"
+          >
+            HOW IT WORKS
+          </a>
+          <a
+            href="#b"
+            className="text-black hover:text-gray-500 text-xs sm:text-sm"
+          >
+            TESTIMONIALS
+          </a>
+          <a
+            href="#c"
+            className="text-black hover:text-gray-500 text-xs sm:text-sm"
+          >
+            PRICING
+          </a>
         </div>
         <div className="flex gap-4 items-center">
-          <button className="py-1 px-2 rounded-full border border-black text-xs sm:text-sm hover:bg-black hover:text-white transition-colors duration-300 font-semibold">ENROLL NOW</button>
-          <button className="py-2 px-4 rounded-full bg-yellow-400 text-white text-xs sm:text-sm hover:bg-yellow-500 transition-colors duration-300 font-semibold">VIEW COURSE PACKAGE</button>
+          <button className="py-1 px-2 rounded-full border border-black text-xs sm:text-sm hover:bg-black hover:text-white transition-colors duration-300 font-semibold">
+            ENROLL NOW
+          </button>
+          <button className="py-2 px-4 rounded-full bg-yellow-400 text-white text-xs sm:text-sm hover:bg-yellow-500 transition-colors duration-300 font-semibold">
+            VIEW COURSE PACKAGE
+          </button>
         </div>
       </div>
     </div>
---
+++ src/components/Image.jsx
@@ -0,0 +1,9 @@
+const Image = () => {
+  return (
+    <div>
+      <img src="/assets/DP.jpg" className="me-3 h-7" alt="Profile" />
+    </div>
+  );
+};
+
+export default Image;
