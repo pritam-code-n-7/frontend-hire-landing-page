319997852f43c2c38b9321da5813489586217201
pritam-code-n-7 <pritamimca1003@gmail.com>
2024-04-09 16:51:13 +0530

    optimize footer section and add lazy attribute to the image tag

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,22 @@
 function Footer() {
+  const lazy = false; 
   const currentDate = new Date().getFullYear();
+
   return (
     <div className="lg:hidden flex flex-col items-center justify-center absolute bottom-0 left-0 right-0 text-[#aaa] mb-40">
       <p>Made by Pritam</p>
       <div className="flex items-center gap-2 mt-2">
         <p className="text-xs">Copyright</p>
-        <img src="/images/copyright.png" className="h-5 w-5" alt="Copyright" />
+        {lazy ? (
+          <div>Loading...</div>
+        ) : (
+          <img
+            src="/images/copyright.png" 
+            loading="lazy" 
+            className="h-5 w-5"
+            alt="Copyright"
+          />
+        )}
         <p className="text-xs">{currentDate}</p>
       </div>
     </div>
